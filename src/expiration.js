90ad5cd795923da546f3cad82d63180bec9e0bae
Fuxian Ding <nekleding@gmail.com>
2015-10-16 16:57:44 +0800

    fix error

--- src/expiration.js
+++ src/expiration.js
@@ -47,7 +47,8 @@ function CcExpController ($scope, $element) {
     } else if (!past && !!expMonth && !!expYear) {
       ngModel.month.$setValidity('ccExpMonth', true)
     }
-
+  }
+  
   this.$watch = function $watchExp () {
     $scope.$watch(function watchExp () {
       return {
